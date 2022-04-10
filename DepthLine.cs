ec515d862bd7a3dfabed0a21887a0bc5c2d33b76
Martin Breen <mjoelbreen@gmail.com>
2022-04-10 15:25:53 -0500

    first commit

---
+++ DepthLine.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+//A tool for measuring distances 
+
+public class DepthLine : MonoBehaviour
+{
+    [SerializeField] LineRenderer line;
+    [SerializeField] GameObject lineStartHandle;
+    [SerializeField] GameObject lineEndHandle;
+    [SerializeField] TextMeshPro heightLabel;
+
+    private float METERS_TO_FEET = 3.2808f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+		Vector3 pointerStart = transform.position;
+        Vector3 pointerDir = Vector3.down;
+		line.SetPosition(0, pointerStart);
+        lineStartHandle.transform.position = pointerStart;
+        lineEndHandle.transform.position = pointerStart;
+
+
+        RaycastHit hitInfo;
+
+		if (Physics.Raycast(pointerStart, pointerDir, out hitInfo, Mathf.Infinity))
+		{
+            var pointerEnd = hitInfo.point;
+            var depth = transform.position.y - pointerEnd.y;
+            var depthInFeet = depth * METERS_TO_FEET;
+
+            line.SetPosition(1, pointerEnd);
+                        lineEndHandle.transform.position = hitInfo.point;
+            heightLabel.text = $"{Mathf.Round(depthInFeet)}'";
+
+
+        } else
+        {
+			line.SetPosition(1, pointerStart + pointerDir * 1000);
+			
+		}
+
+		
+        
+    }
+}
---
+++ Selectable.cs
@@ -0,0 +1,68 @@
+﻿using EasyButtons;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+//For selecting UI elements in VR
+
+public enum SELECT_EVENT
+{
+    IDLE,
+    ON_HOVER_START,
+    ON_HOVER_STAY,
+    ON_HOVER_END,
+}
+
+public class Selectable : MonoBehaviour
+{
+    [SerializeField] UnityEvent onSelectEvent;
+    [SerializeField] UnityEvent onHoverBegin;
+    [SerializeField] UnityEvent onHoverEnd;
+
+    public SELECT_EVENT currentState = SELECT_EVENT.IDLE;
+
+    void Awake() { }
+
+    void Update() { }
+
+    [Button]
+    public void Select()
+    {
+        if (this.enabled)
+            onSelectEvent.Invoke();
+    }
+
+    public void OnHoverBegin()
+    {
+        if (enabled)
+        {
+            if (currentState == SELECT_EVENT.IDLE || currentState == SELECT_EVENT.ON_HOVER_END)
+            {
+                currentState = SELECT_EVENT.ON_HOVER_START;
+                onHoverBegin.Invoke();
+                currentState = SELECT_EVENT.ON_HOVER_STAY;
+            }
+        }
+    }
+
+
+    public void OnHoverEnd()
+    {
+        if (this.enabled)
+        {
+            if (currentState == SELECT_EVENT.ON_HOVER_STAY || currentState == SELECT_EVENT.ON_HOVER_START)
+            {
+                currentState = SELECT_EVENT.ON_HOVER_END;
+                onHoverEnd.Invoke();
+                currentState = SELECT_EVENT.IDLE;
+            }
+        }
+    }
+
+    public void Disable()
+    {
+        onHoverEnd.Invoke();
+        //currentState = SELECT_EVENT.IDLE;
+        enabled = false;
+    }
+}
